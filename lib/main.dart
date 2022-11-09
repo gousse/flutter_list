51b68d04e4019dc30a4def408dc6d4cf2d29c494
Bruno Munoz <bruno.munoz@orange.com>
2022-11-09 15:12:09 +0100

    list

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Demo list and grid'),
+      home: const MyHomePage(title: 'Demo list'),
     );
   }
 }
